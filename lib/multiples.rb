ef717299d60065ded2e0b0d7de287ea4e3aca4f6
corya0687 <corya0687@gmail.com>
2015-12-03 10:39:50 +0000

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,32 @@
-# Enter your procedural solution here!
+def create_matrix(matrix)
+  i=0
+  while i < matrix.size
+  matrix[i]= i+1
+  i  += 1
+  end
+end
+
+def collect_multiples(matrix_size)
+  matrix = Array.new(((matrix_size.to_i)-1),0)
+  create_matrix(matrix)
+  puts create_matrix(matrix)
+  matrix.select{|x| x % 3 == 0 || x % 5 == 0}
+end
+
+def add_up_array(new_matrix)
+  i=0
+  sum_three_fives = 0
+  until i == new_matrix.size
+  sum_three_fives += new_matrix[i]
+  i += 1
+  end
+  sum_three_fives
+end
+
+def sum_multiples(matrix_size)
+  new_matrix= collect_multiples(matrix_size)
+  add_up_array(new_matrix)
+  end
+
+#puts "#{matrix}"
+ #[0..1000]
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,43 @@
-# Enter your object-oriented solution here!
+class Multiples
+  def initialize(matrix_size)
+    @matrix_size= matrix_size
+    @matrix = Array.new(((@matrix_size.to_i)-1),0)
+  end
+
+  def matrix_size= (matrix_size)
+    @matrix_size= matrix_size
+  end
+
+  def matrix_size
+    @matrix_size
+  end
+
+  def create_matrix
+  i=0
+    @matrix
+    while i < @matrix_size - 1
+    @matrix[i]= i+1
+    i  += 1
+    end
+  end
+
+  def collect_multiples
+    create_matrix
+    @matrix.select{|x| x % 3 == 0 || x % 5 == 0}
+  end
+
+  def add_up_array(new_matrix)
+    i=0
+    sum_three_fives = 0
+    until i == new_matrix.size
+    sum_three_fives += new_matrix[i]
+    i += 1
+  end
+    sum_three_fives
+  end
+
+  def sum_multiples
+    new_matrix= collect_multiples
+    add_up_array(new_matrix)
+  end
+end
